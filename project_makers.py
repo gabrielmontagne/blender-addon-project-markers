7ee2e7aa2a719c59a5ef6d002e5897b5ad62dd03
gabriel <gabriel@gorgias.ranchoriedad.local>
2020-04-24 17:37:50 +0100

    add operator to project start /current /end frames to other scenes

--- project_makers.py
+++ project_makers.py
@@ -12,16 +12,13 @@ bl_info = {
 
 class PROJECT_MAKER_OT_tibasenno(bpy.types.Operator):
     """Replace all makers in other scenes with the markers in this scene"""
-    bl_idname = "marker.project_to_scenes"
+    bl_idname = "marker.project_markers_to_scenes"
     bl_label = "Project markers to other scenes"
 
     def execute(self, context):
-        print('Project markers to other scene')
+        print('Project markers to other scenes')
         scene = context.scene
         markers = scene.timeline_markers
-
-        for m in markers:
-            print('m', m)
 
         for s in bpy.data.scenes:
             if s is scene:
@@ -35,12 +32,35 @@ class PROJECT_MAKER_OT_tibasenno(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class PROJECT_START_END_OT_tibasenno(bpy.types.Operator):
+    """Replace all makers in other scenes with the markers in this scene"""
+    bl_idname = "marker.project_start_end_to_scenes"
+    bl_label = "Project start / current / end frames to other scenes"
+
+    def execute(self, context):
+        print('Project start-end to other scenes')
+        scene = context.scene
+        markers = scene.timeline_markers
+
+        for s in bpy.data.scenes:
+            if s is scene:
+                continue
+
+            s.frame_start = scene.frame_start
+            s.frame_current = scene.frame_current
+            s.frame_end = scene.frame_end
+            s.frame_preview_end = scene.frame_preview_end
+            s.frame_preview_start = scene.frame_preview_start
+
+        return {'FINISHED'}
+
 def register():
     bpy.utils.register_class(PROJECT_MAKER_OT_tibasenno)
+    bpy.utils.register_class(PROJECT_START_END_OT_tibasenno)
 
 def unregister():
+    bpy.utils.unregister_class(PROJECT_START_END_OT_tibasenno)
     bpy.utils.unregister_class(PROJECT_MAKER_OT_tibasenno)
 
 if __name__ == "__main__":
     register()
-    bpy.ops.marker.project_to_scenes()
